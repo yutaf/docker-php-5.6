1936bc63c3c68543ef9c3beb1d65f04a916afcf6
yutaf <yutafuji2008@gmail.com>
2015-07-17 18:11:04 +0900

    Cosme

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-#TODO set TERM in bashrc or something
-export TERM=xterm-256color
-
 # php session
 mkdir -m 777 -p /srv/php/session
 echo "tmpfs /srv/php/session tmpfs size=32m,mode=700,uid=daemon,gid=daemon 0 0" >> /etc/fstab
