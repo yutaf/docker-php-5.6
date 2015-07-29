8a194bb200fdd48a18a3e8301431f22696d1015a
yutaf <yutafuji2008@gmail.com>
2015-07-29 11:35:59 +0900

    Make log directories after Dockerfile build

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,6 +8,14 @@ mount /srv/php/session # need docker run "--cap-add=SYS_ADMIN" option
 # php upload_tmp_dir
 mkdir -m 777 -p /srv/php/upload_tmp_dir
 
+# make log directories
+test -d /srv/www/logs/access || mkdir -m 777 -p /srv/www/logs/access
+test -d /srv/www/logs/error || mkdir -m 777 -p /srv/www/logs/error
+test -d /srv/www/logs/app || mkdir -m 777 -p /srv/www/logs/app
+cd /srv/www/logs
+chmod 777 access error app
+cd -
+
 #
 # Run container foreground
 #
