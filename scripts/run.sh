22c6825a044118fba6fce37ff6fb9ed76a21923f
yutaf <yutafuji2008@gmail.com>
2015-12-04 14:13:09 +0900

    Fix tmp mount user from daemon to apache

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 
 # php session
 mkdir -m 777 -p /srv/php/session
-echo "tmpfs /srv/php/session tmpfs size=32m,mode=700,uid=daemon,gid=daemon 0 0" >> /etc/fstab
+echo "tmpfs /srv/php/session tmpfs size=32m,mode=700,uid=apache,gid=apache 0 0" >> /etc/fstab
 mount /srv/php/session # need docker run "--cap-add=SYS_ADMIN" option
 
 # php upload_tmp_dir
